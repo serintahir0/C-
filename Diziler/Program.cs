a28eb94a5ec28d79f96e9a60f938cf1266e3c380
serintahir0 <103417001+serintahir0@users.noreply.github.com>
2022-09-09 23:03:18 +0300

    www.patika.dev ödev 9
    www.patika.dev ödev 9

---
+++ Diziler/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace degisken{
+
+    class Program{
+        static void Main(string[] args){
+            
+            /*
+            string[] renkler = new string[5]; //eleman sayısı belli, elemanları belli olmayan dizi tanımlama
+            string[] hayvanlar = {"kedi","köpek", "kuş", "maymun"}; //elemanları belli olan dizi tanımlama (eleman sayısı bellidir)
+
+            int[] dizi;
+            dizi = new int[5]; //diziyi önden oluşturma ve sonradan eleman sayısını tanımlama
+
+            renkler[0] = "mavi"; //dizi elemanı ekleme (elemana string atama)
+            dizi[3] = 3 ; //dizi elemanına integer değer atama
+            
+            */
+
+            //döngülerle dizi kullanımı
+
+            //klavyeden girilen n tane sayının ortalamasını alan program
+            
+           
+            Console.WriteLine("Lütfen dizinin eleman sayisini giriniz:");
+            int diziUzunlugu = int.Parse(Console.ReadLine());
+            int[] sayiDizisi= new int[diziUzunlugu];
+            
+
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.WriteLine("Lütfen {0}. elemani giriniz:",i+1);
+                sayiDizisi[i] = int.Parse(Console.ReadLine());
+            }
+
+            int toplam = 0;
+
+            foreach (var sayi in sayiDizisi)
+            {
+                toplam += sayi;
+            }
+            Console.WriteLine("Ortalama:" + toplam/diziUzunlugu);
+
+        }
+    }
+}
