acd83837d07a0e6e4f1a0f792306002ef6ee0729
serintahir0 <103417001+serintahir0@users.noreply.github.com>
2022-09-09 17:43:51 +0300

    www.patika.dev ödev 6
    www.patika.dev ödev 6

---
+++ Switch_Case/switch_case.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace degisken{
+
+    class Program{
+        static void Main(string[] args){
+            
+            int month = DateTime.Now.Month;
+
+            switch (month)
+            {
+                case 1:{
+                   Console.WriteLine("Ocak Ayındasınız.");
+                    break; 
+                }
+                case 2:{
+                   Console.WriteLine("Şubat Ayındasınız.");
+                    break; 
+                }
+                case 3:{
+                   Console.WriteLine("Mart Ayındasınız.");
+                    break; 
+                }
+                //Not case'lerin sırasının önemi yoktur. Şart hangi case'e uyuyorsa o çalışır.
+                case 6:
+                case 7:
+                case 8:{
+                    Console.WriteLine("Yaz mevsimindesiniz");
+                    break;
+                }
+                //3 farklı case'de aynı işlemi yapmak istiyorsak kodu bu şekilde yazarı.
+                
+
+                
+                default:{
+                    Console.WriteLine("Yanlış veri girişi");
+                }
+                //default -> hiçbir case'e uymayan işlemleri burda yaptırırız.
+                break;
+            }
+
+        }
+    }
+}
