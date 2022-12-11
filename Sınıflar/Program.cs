913c3a8e7de6f606750fb56bd7f6c20cbcb276dd
serintahir0 <103417001+serintahir0@users.noreply.github.com>
2022-12-11 16:16:28 +0300

    www.patika.dev ödev 18
    www.patika.dev ödev 18

---
+++ Sınıflar/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Patika{
+
+    class Siniflar{
+        static void Main(string[] args){
+             
+            // Söz Dizimi
+            // class SinifAdi
+            // {
+            //      Erişim_Belirleyici Veri_Tipi Özellik_Adı;
+            //      Erişim_Belirleyici Geri_Dönüş_Tipi Metot_Adı(Parametreler);
+            //      {
+            //          Komutlar
+            //      }
+
+            // Erişim Belirleyiciler
+            // Public : her yerden erişilebilen metotlar Public tanımlanır
+            // Private : sadece tanımlandığı sınıf içinden erişilebilen metotlar Private tanımlanır
+            // Internal : sadece kendi bulunduğu proje içerisinden erişilebilen metotlar Internal tanımlanır
+            // Protected : sadece tanımlandığı sınıfta veya o sınıftan kalıtım alan sınıflarda kullanılabilen metotlar Protected tanımlanır
+
+            Calisan calisan1 = new Calisan();
+            calisan1.Ad = "Ayşe";
+            calisan1.Soyad ="Kara";
+            calisan1.No = 29150234;
+            calisan1.Departman = "İnsan Kaynakları";
+
+            calisan1.CalisanBilgileri();
+        }
+    }
+
+    class Calisan{
+        public string Ad;
+        public string Soyad;
+        public string Departman;
+        public int No;
+
+        public void CalisanBilgileri(){
+            Console.WriteLine("Çalışan Adı:{0}",Ad);
+            Console.WriteLine("Çalışan Soyadı:{0}",Soyad);
+            Console.WriteLine("Çalışan Numarası:{0}",No);
+            Console.WriteLine("Çalışan Departmanı:{0}",Departman);
+
+
+        }
+    }
+}
