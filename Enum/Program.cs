ef7d783b572fcf41476ba7e68a419c57d0672cac
serintahir0 <103417001+serintahir0@users.noreply.github.com>
2022-12-12 09:38:46 +0300

    www.patika.dev ödev 23
    www.patika.dev ödev 23

---
+++ Enum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Patika{
+
+    class Enum{
+        static void Main(string[] args){
+            //"enum" anahtar kelimesi enumeration yani numaralandırma kelimesinden gelir. 
+            //Sayısal verilerı string ifadelerle saklamanızı sağlar. 
+            //Okunabilirliğe katkısı da tam olarak burdan gelir diyebiliriz.
+            
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Cumartesi); //Cumartesi elemanının sayısal verisine erişmek istersek bu şekilde tip dönüşümü yaparız
+            //cumaya 23 atarsak cumartesi ardışık olarak 24 alır
+
+            Console.WriteLine("Havanın derecesini girin: ");
+            int sicaklik = Convert.ToInt32(Console.ReadLine());
+
+            if(sicaklik<=(int)HavaDurumu.normal){
+                Console.WriteLine("Dışarı çıkmak için havanın ısınmasını bekle");
+            }
+            else if(sicaklik>=(int)HavaDurumu.sicak){
+                Console.WriteLine("Dışarı çıkmak için çok sıcak bir gün");
+            }
+            else if(sicaklik>=(int)HavaDurumu.normal && sicaklik<=(int)HavaDurumu.coksicak){
+                Console.WriteLine("Hadi dışarı çıkalım");
+
+            }
+        }
+    }
+    
+    enum Gunler{
+        Pazartesi,
+        Sali,
+        Carsamba,
+        Persembe,
+        Cuma=23,
+        Cumartesi,
+        Pazar
+    }
+
+    enum HavaDurumu{
+        normal=20,
+        sicak=25,
+        coksicak=30,
+        soguk=-5
+    }
+}
